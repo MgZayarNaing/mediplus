8cc0c0977f53244769b0fe7c2f693afea9820da0
MgZayarNaing <zayarnaing.st@gmail.com>
2024-10-24 11:46:49 +0630

    base

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def Home(request):
+    imageslider = ImagesliderModel.objects.all().order_by('-id')
+    return render(request,'index.html', {'imageslider':imageslider})
